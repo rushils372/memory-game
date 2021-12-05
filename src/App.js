73233458ccf10fce23c01ed2f3bf82b09d553469
rushils372 <59312459+rushils372@users.noreply.github.com>
2021-12-05 17:54:19 +0530

    Gadget Version 1.1.1.1

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Magic Match</h1>
+      <h1 className="tracking-in-expand">Gadget Match</h1>
       <button onClick={shuffleCards}>New Game</button>
 
       <div className="card-grid">
